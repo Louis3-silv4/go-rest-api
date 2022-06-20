fc783bd92d4b7fced6455281462aa8d63bbfb282
louise <louise.silva@g.globo>
2022-06-20 14:36:07 -0300

    Controle da pagina

---
+++ controllers/controllers.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"github.com/Louis3-silv4/go-rest-api/database"
+	"github.com/Louis3-silv4/go-rest-api/models"
+	"github.com/gorilla/mux"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+}
+
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	var p []models.Personalidade
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
+}
+
+func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id )
+	json.NewEncoder(w).Encode(personalidade)
+}
